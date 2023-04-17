55f5951f1bc83fc7abd37aa673335eea9a0c5cce
Hassan Dia <hassandia_@outlook.com>
2023-04-17 15:25:07 +0200

    Request test in linkedin.py

--- Project/code/linkedIn.py
+++ Project/code/linkedIn.py
@@ -1,1 +1,10 @@
 # All code explicit for webscraping LinkedIn.com
+
+import requests
+
+print("before")
+url = "https://www.linkedin.com/jobs/search/?currentJobId=3560018027&geoId=104853962&keywords=engineer&location=Stockholm%20County%2C%20Sweden&refresh=true"
+response = requests.get(url)
+ 
+print(response.content)
+print("after")
