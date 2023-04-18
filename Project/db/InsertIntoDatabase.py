ca3a487af9ae061a8eff9615544ae7b39fdaa212
Axel Lindqvist <axel.lindqvist@hotmail.se>
2023-04-18 14:25:31 +0200

    Wrapped the code into a  function

--- Project/db/InsertIntoDatabase.py
+++ Project/db/InsertIntoDatabase.py
@@ -17,10 +17,8 @@ y = str(4);
 date = TO_DATE(date)
 
 argplaceholder = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-job_param_list = argplaceholder[0:8] # Get a sub-list from index 0 to 7
-#job_param_list is the list of arguments for the job_listing table.
-delimiter = ","
-liststr = delimiter.join(job_param_list)
+
+
 
 county = "placeholder"
 
@@ -31,9 +29,7 @@ work_hours = "placeholder"
 
 
 
-#SQL commands to insert the job_listing, multiple ones because the information is spread among multiple tables.
-# Also some values derived after insert functions 
-Insert_job_listing = "INSERT INTO job_listing(source, employment_type, duration, min_salary, max_salary, salary_type, publication_date, job_id, location_id, date_gathered) VALUES (" + job_param_list + ");"
+
 
 #440 bits = 55 bytes per row.
 
@@ -61,7 +57,7 @@ get_work_hours_id = "SELECT SCOPE_IDENTITY();"
 #TODO Make code to execute previous SQL commands.
 job_listing_id = "placeholder"
 requirement_id = "placeholder"
-Insert_requirement_relation = "INSERT INTO requirement_relation(job_listing_id, requirement_id) VALUES (" + job_listing_id + "," + requirement_id + ");"
+
 
 
 work_hours_id = "placeholder"
@@ -94,13 +90,78 @@ pd.read_sql_query(Insert_job_listing,sql_connect)
 
 sql_connect.close()
 
-
 #Extract Data from DB
 
 # SELECT COUNT(id) FROM job_listing WHERE location_id IN (SELECT location_id FROM location WHERE county IN ("lan") );
 
 # Displays the number of jobs in each county
 # SELECT COUNT(id), location.county FROM job_listing INNER JOIN location ON location.id = job_listing.location_id GROUP BY county;
+
+def insert_data(argument_list):
+    job_param_list = argument_list[0:8] # Get a sub-list from index 0 to 7
+    #job_param_list is the list of arguments for the job_listing table.
+    delimiter = ","
+    liststr = delimiter.join(job_param_list)
+
+    #SQL commands to insert the job_listing, multiple ones because the information is spread among multiple tables.
+    # Also some values derived after insert functions 
+    #
+    #Get id has to be executed right after the row has been inserted.
+
+    #getting job id for job_listing insert
+    job_exists_query = "SELECT * FROM job WHERE job = " + argument_list[7] + ";"
+
+    job_exist_result = cursor.execute(job_exists_query).fetchall()
+
+    if bool(job_exist_result):
+        insert_job = "INSERT INTO job(job) VALUES (" + argument_list[7] + ");"
+        pd.read_sql_query(insert_job,sql_connect)
+
+
+    get_job_id = "SELECT id FROM job WHERE job = " + argument_list[7] + ";"
+    job_id = cursor.execute(get_job_id).fetchall()[0]
+
+    #TODO getting location id for job_listing insert and creating location if it doesn't exist
+    
+
+    #Getting work_hours_id 
+
+
+
+
+    #Create Job listing here and get job_listing_id
+    #TODO Make sure publication date is in a uniform format
+    insert_job_listing = "INSERT INTO job_listing(source, employment_type, duration, min_salary, max_salary, salary_type, publication_date, job_id, location_id, date_gathered) VALUES (" + job_param_list + ");"
+    job_listing_id = cursor.execute(insert_job_listing).fetchall()[0]
+
+
+
+
+    for i in argument_list[10]:
+        requirement_exist_query = "SELECT id FROM requirement WHERE requirement = " + i[0] + " AND years_of_experience = " + i[1] + ";"
+        requirement_exist_result = cursor.execute(requirement_exist_query).fetchall()
+        if bool(requirement_exist_result):
+            Insert_requirement = "INSERT INTO requirement(requirement, years_of_experience) VALUES ( + " + i[0] + "," + i[1] + ");"
+            pd.read_sql_query(Insert_requirement, sql_connect)
+        
+        get_requirement_id = "SELECT id FROM requirement WHERE requirement = " + i[0] + " AND years_of_experience = " + i[1] + ";"
+        requirement_id = cursor.execute(get_requirement_id).fetchall()[0]
+
+        #Creates row for many to many relation table requirement_relation
+        insert_requirement_relation = "INSERT INTO requirement_relation(job_listing_id, requirement_id) VALUES (" + job_listing_id + "," + requirement_id + ");"
+        cursor.execute(insert_requirement_relation, sql_connect)
+
+
+    
+
+
+
+
+    
+    
+
+
+
 
 
 
