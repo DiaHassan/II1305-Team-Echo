e6135216be49e0db7c8e01b1391191015e9d8eb8
ZakOra1 <32168076+ZakOra1@users.noreply.github.com>
2023-04-18 15:51:20 +0200

    Deleted redundant files, code and added comments

--- Project/code/platsbanken/bootstrap.py
+++ Project/code/platsbanken/bootstrap.py
@@ -1,7 +1,7 @@
 import sys
 import logging
-import time_handling
-from get_ads import get_ads
+import datetime
+from get_ads import get_ads, date
 from database import load_all, DBConnectionHandler
 
 from settings import LOG_LEVEL, LOG_DATE_FORMAT, LOG_FORMAT, DB_TABLE_NAME, PLACES, OCCUPATIONS, DB_FILE_NAME
@@ -13,19 +13,11 @@ if __name__ == '__main__':
     
     # This is executed once to create the database and 
     # load all ads into it 
-    # To keep the database updated, run main.py   
-    
+
     with DBConnectionHandler() as db:
         db.create_db_table()
     log.info(f'Database created: "{DB_FILE_NAME}"')
 
-    if PLACES or OCCUPATIONS:
-        # if you want to keep a database with a subset of all ads based on geography or occupations,
-        # there is no need to download all ads, but timestamp needs to be in the past
-        # so that the first update will work
-        timestamp = time_handling.write_timestamp('2023-03-01T00:00:00')
-    else:
-        timestamp = time_handling.write_timestamp()
-        all_ads = get_ads()
-        load_all(all_ads)
-        log.info(f'Loaded {len(all_ads)} into the database table "{DB_TABLE_NAME}". Timestamp: {timestamp}')
+    all_ads = get_ads()
+    load_all(all_ads)
+    log.info(f'Loaded {len(all_ads)} into the database table "{DB_TABLE_NAME}". Timestamp: {date}')
--- Project/code/platsbanken/get_ads.py
+++ Project/code/platsbanken/get_ads.py
@@ -9,17 +9,26 @@ from settings import LOG_LEVEL, LOG_DATE_FORMAT, LOG_FORMAT, DATE_FORMAT, STREAM
 log = logging.getLogger(__name__)
 logging.basicConfig(stream=sys.stdout, level=LOG_LEVEL, format=LOG_FORMAT, datefmt=LOG_DATE_FORMAT)
 
+date = 0
+
 # Main method updating the list of job ads
 def get_ads():
+
+    # Declare variables
     url = STREAM_URL
     date = datetime.datetime.now() - datetime.timedelta(1)
     params = {'date': date.strftime(DATE_FORMAT)}
+
+    # If the search query requires PLACE or OCCUPATION ()
     if PLACES:
         params['location-concept-id'] = PLACES
     if OCCUPATIONS:
         params['occupation-concept-id'] = OCCUPATIONS
 
+    # Writing log info to the terminal
     log.info(f'Collecting ads from: {url} with params {params}')
+
+    # Send GET request to JobStream API 
     headers = {'Accept': 'application/json'}
     response = requests.get(url, headers=headers, params=params)
     response.raise_for_status()
