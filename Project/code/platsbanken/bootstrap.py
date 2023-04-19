697a14fe7af392bdfcd4d33af86a5c073a9fe018
ZakOra1 <32168076+ZakOra1@users.noreply.github.com>
2023-04-19 09:02:37 +0200

    Fixed PB API

--- Project/code/platsbanken/bootstrap.py
+++ Project/code/platsbanken/bootstrap.py
@@ -1,23 +1,17 @@
 import sys
 import logging
-import datetime
 from get_ads import get_ads, date
-from database import load_all, DBConnectionHandler
+from database import load_all
+from settings import LOG_LEVEL, LOG_DATE_FORMAT, LOG_FORMAT
 
-from settings import LOG_LEVEL, LOG_DATE_FORMAT, LOG_FORMAT, DB_TABLE_NAME, PLACES, OCCUPATIONS, DB_FILE_NAME
 
+# Logging
 log = logging.getLogger(__name__)
 logging.basicConfig(stream=sys.stdout, level=LOG_LEVEL, format=LOG_FORMAT, datefmt=LOG_DATE_FORMAT)
 
+
+# Main script
 if __name__ == '__main__':
-    
-    # This is executed once to create the database and 
-    # load all ads into it 
-
-    with DBConnectionHandler() as db:
-        db.create_db_table()
-    log.info(f'Database created: "{DB_FILE_NAME}"')
-
     all_ads = get_ads()
-    load_all(all_ads)
-    log.info(f'Loaded {len(all_ads)} into the database table "{DB_TABLE_NAME}". Timestamp: {date}')
+    list = load_all(all_ads)
+    log.info(f'Loaded {len(all_ads)}. Timestamp: {date}')
--- Project/code/platsbanken/get_ads.py
+++ Project/code/platsbanken/get_ads.py
@@ -3,20 +3,24 @@ import json
 import logging
 import requests
 import datetime
-
 from settings import LOG_LEVEL, LOG_DATE_FORMAT, LOG_FORMAT, DATE_FORMAT, STREAM_URL, PLACES, OCCUPATIONS
 
+
+# Logging
 log = logging.getLogger(__name__)
 logging.basicConfig(stream=sys.stdout, level=LOG_LEVEL, format=LOG_FORMAT, datefmt=LOG_DATE_FORMAT)
 
+
+# For bootstrap.py
 date = 0
 
-# Main method updating the list of job ads
+
+# Retrieves all ads in full
 def get_ads():
 
     # Declare variables
     url = STREAM_URL
-    date = datetime.datetime.now() - datetime.timedelta(1)
+    date = datetime.datetime.now() - datetime.timedelta(8)
     params = {'date': date.strftime(DATE_FORMAT)}
 
     # If the search query requires PLACE or OCCUPATION ()
@@ -33,6 +37,7 @@ def get_ads():
     response = requests.get(url, headers=headers, params=params)
     response.raise_for_status()
     list_of_ads = json.loads(response.content.decode('utf8'))
+
     log.info(f"Got {len(list_of_ads)} ads from {url}. Params: {params}")
 
     return list_of_ads
