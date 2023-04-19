6c63b589b2eb24e66ff9fdeeaa5abbccedcdf105
DrakenDurfors <47025935+DrakenDurfors@users.noreply.github.com>
2023-04-19 14:36:03 +0200

    Added database creation script to db_connect.py

---
+++ Project/db/db_connect.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+def connect():
+    connection = sqlite3.connect("echo.db")
+    return connection
+
+def build():
+    connection = connect()
+    cursor = connection.cursor()
+
+    sql_file = open("Project\db\db_sqlite.sql", "r")
+    sql_script = sql_file.read()
+
+    cursor.executescript(sql_script)
+    cursor.close()
+    connection.close()
+
+
+if __name__ == '__main__':
+    build()
