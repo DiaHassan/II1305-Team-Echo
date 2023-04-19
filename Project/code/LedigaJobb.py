3b65be3454c5ca045fb31399c4f71ee00e99a287
Hassan Dia <hassandia_@outlook.com>
2023-04-19 11:25:55 +0200

    Updating Ledigajobb.py
    The added functionalities are:
    Getting job links
    Getting the HTML code
    Creating proper URLs
    Editing URLs
    Going through the different pages of a job search

--- Project/code/LedigaJobb.py
+++ Project/code/LedigaJobb.py
@@ -14,8 +14,8 @@ def get_code(url):
     soup = BeautifulSoup(response.content, 'html.parser')
     return soup
 
+# Find all the job listings on the page
 def get_jobs(response):
-    # Find all the job listings on the page
     job_listings = response.find_all('div', {'class': 'posting-title'})
     return job_listings
 
@@ -24,17 +24,44 @@ def get_job_links(job_listings):
     # Loop through each job listing and print the job title and company name
     for job in job_listings:
         job_link = job.find('a').get("href")
-        print(job_link)
+        # print(job_link)
 
+# Get the link to the next page
 def get_next_page(response):
     next_btn_link = response.find('a', {'class': 'page-link', 'aria-label': 'Next'}).get("href")
     return next_btn_link
 
+# Creates link to initiate search 
+def create_search_link(lan, work, page):
+    new_string = f"https://ledigajobb.se/sok?cc={lan}&s={work}&p={page}"
+    return new_string
+
+# Replacing the index of the link to get to the next page
+def replace_after(my_string, to_replace, replacement):
+    index = my_string.find(to_replace) + len(to_replace)
+    new_string = my_string[:index] + replacement + my_string[index+1:]
+    # print(new_string) 
+
+def join_url(base_url, section):
+    return base_url + section
+
+    
+##################################################
 # Main function for testing the code
 def main():
-    response = get_code(search_url)
-    job_listings = get_jobs(response)
-    print(get_next_page(response))
+    response = get_code("https://ledigajobb.se/jobb/a752c9/automationstekniker-till-nytt-omr%C3%A5de-hos-combitech-i-stockholm")
+    job_listings = response.find_all('div', {'class': 'posting-title'})
+    print(job_listings)
+    # response = get_code(search_url)
+    # temp = create_search_link(9,"utvecklare",3)
+    # print(temp)
+    # response = get_code(temp)
+    # print("\n")
+    # print(get_job_links(get_jobs(response)))
+    # print("\n")
+    # job_listings = get_jobs(response)
+    # print("\n")
+    # print(get_next_page(response))
 
 
 if __name__ == '__main__':
