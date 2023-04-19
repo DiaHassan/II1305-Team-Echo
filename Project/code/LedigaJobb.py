55d2855285dbfcbb73742a233ff227ef924efabb
Hassan Dia <hassandia_@outlook.com>
2023-04-19 13:06:32 +0200

    Update LedigaJobb.py

--- Project/code/LedigaJobb.py
+++ Project/code/LedigaJobb.py
@@ -14,10 +14,12 @@ def get_code(url):
     soup = BeautifulSoup(response.content, 'html.parser')
     return soup
 
+
 # Find all the job listings on the page
 def get_jobs(response):
     job_listings = response.find_all('div', {'class': 'posting-title'})
     return job_listings
+
 
 # Get all jobs
 def get_job_links(job_listings):
@@ -26,21 +28,25 @@ def get_job_links(job_listings):
         job_link = job.find('a').get("href")
         # print(job_link)
 
+
 # Get the link to the next page
 def get_next_page(response):
     next_btn_link = response.find('a', {'class': 'page-link', 'aria-label': 'Next'}).get("href")
     return next_btn_link
+
 
 # Creates link to initiate search 
 def create_search_link(lan, work, page):
     new_string = f"https://ledigajobb.se/sok?cc={lan}&s={work}&p={page}"
     return new_string
 
+
 # Replacing the index of the link to get to the next page
 def replace_after(my_string, to_replace, replacement):
     index = my_string.find(to_replace) + len(to_replace)
     new_string = my_string[:index] + replacement + my_string[index+1:]
     # print(new_string) 
+
 
 def join_url(base_url, section):
     return base_url + section
@@ -50,8 +56,16 @@ def join_url(base_url, section):
 # Main function for testing the code
 def main():
     response = get_code("https://ledigajobb.se/jobb/a752c9/automationstekniker-till-nytt-omr%C3%A5de-hos-combitech-i-stockholm")
-    job_listings = response.find_all('div', {'class': 'posting-title'})
-    print(job_listings)
+    # job_listings = response.find_all('div', class_='col bg-light rounded-bottom pb-2 border-top')
+    # temp = job_listings.find('div')
+    # print(temp)
+
+    outer_div = response.find('div', class_='col bg-light rounded-bottom pb-2 border-top').find('div').find('div').find('ul').find_all('li')
+
+    for i in outer_div:
+        print(i.text)
+    
+    # print(outer_div)
     # response = get_code(search_url)
     # temp = create_search_link(9,"utvecklare",3)
     # print(temp)
