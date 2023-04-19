a13ca1b508046bc036bcff83217298a7fd9b1e81
ZakOra1 <32168076+ZakOra1@users.noreply.github.com>
2023-04-19 09:59:56 +0200

    Merged bootstrap, get_ads and database to one file

--- Project/code/platsbanken/platsbanken.py
+++ Project/code/platsbanken/platsbanken.py
@@ -1,9 +1,13 @@
-import sys
-import json
-import logging
 import requests
 import datetime
+import logging
+import json
+import sys
 from settings import LOG_LEVEL, LOG_DATE_FORMAT, LOG_FORMAT, DATE_FORMAT, STREAM_URL, PLACES, OCCUPATIONS
+
+
+# Global variable
+date = 0
 
 
 # Logging
@@ -11,8 +15,25 @@ log = logging.getLogger(__name__)
 logging.basicConfig(stream=sys.stdout, level=LOG_LEVEL, format=LOG_FORMAT, datefmt=LOG_DATE_FORMAT)
 
 
-# For bootstrap.py
-date = 0
+# Loads all ads into a list with appropiate parameters
+def load_all(all_ads):
+    list = []
+    for ad in all_ads:
+      list.append(insert_one_ad(ad)) 
+    log.debug(f'Insert multiple ads: ({len(all_ads)} ads)')
+    print(list[0])
+    print(list[500])
+    print(list[1000])
+    return list
+
+
+# Creates a list for one ad with correct parameters
+def insert_one_ad(ad):
+    ad_id = ad['id']
+    email = ad.get('application_details', {}).get('email', ' ')
+    city = ad.get('workplace_address', {}).get('municipality', ' ')
+    occupation = ad.get('occupation', {}).get('label', ' ')
+    return [ad_id, email, city, occupation]
 
 
 # Retrieves all ads in full
@@ -41,3 +62,10 @@ def get_ads():
     log.info(f"Got {len(list_of_ads)} ads from {url}. Params: {params}")
 
     return list_of_ads
+
+
+# Main script
+if __name__ == '__main__':
+    all_ads = get_ads()
+    list = load_all(all_ads)
+    log.info(f'Loaded {len(all_ads)}. Timestamp: {date}')
