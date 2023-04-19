d5852b9258a8813f2f11e2afdaab01dae8855989
EdwinAhl <edwin.ahlstrand@bahnhof.se>
2023-04-19 11:19:56 +0200

    Added template for main.py, deleted indeed.py

--- Project/code/main.py
+++ Project/code/main.py
@@ -1,1 +1,19 @@
-# Main file that handles everything else
+from platsbanken import platsbanken
+from db import InsertIntoDatabase
+
+
+# Platsbanken
+platsbanken_list = platsbanken.run()
+InsertIntoDatabase.insert_data(platsbanken_list)
+
+# LedigaJobb
+# ledigajobb_list = ...
+# InsertIntoDatabase.insert_data(ledigajobb_list)
+
+# LinkedIn
+# linkedIn_list = ...
+# InsertIntoDatabase.insert_data(linkedIn_list)
+
+
+
+
