f61d2680d73372af7587d0cb122b3d3ec4b8d539
Hassan Dia <hassandia_@outlook.com>
2023-04-19 16:31:40 +0200

    Resolved file clash

--- Project/code/ledigajobb.py
+++ Project/code/ledigajobb.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from requirementfinder import find_req
-
+import csv
 
 # Define the URL to scrape
 base_url = 'https://ledigajobb.se'
@@ -121,9 +121,16 @@ def main():
     # response = get_code("https://ledigajobb.se/jobb/a4c766/trainee-backend-utvecklare")
     
     # print(scrape_ad("https://ledigajobb.se/jobb/a7ed79/nynas-s%C3%B6ker-tv%C3%A5-processingenj%C3%B6rer-omg%C3%A5ende"))
-    for i in range(2, 21):
-        print(create_search_link(i,"utvecklare",2))
-        
+    county_data = []
+
+    with open('lan.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            county_code = row[0].split()[0]
+            county_name = ' '.join(row[0].split()[1:])
+            county_data.append((county_code, county_name))
+
+    print(county_data)
 
 if __name__ == '__main__':
     main()
