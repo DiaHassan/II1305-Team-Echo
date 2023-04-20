fc59c79e7af7f70bb88b25a0f5fb67d17d682cfb
ZakOra1 <32168076+ZakOra1@users.noreply.github.com>
2023-04-20 09:59:43 +0200

    Added experience field to platsbanken.py

--- Project/code/platsbanken/platsbanken.py
+++ Project/code/platsbanken/platsbanken.py
@@ -18,6 +18,18 @@ def run():
 
    # Creates a 2d list of ads containting sought parameters
    list = extract_data_all_ads(all_ads)
+   #print(list[0])
+   #print(list[500])
+   #print(list[1000])
+   flag = True
+   i = 0
+   while flag:
+    if list[i][6] != []:
+        break
+    i += 1
+    
+   print(list[i])    
+
    return list
 
 
@@ -54,11 +66,28 @@ def extract_data_all_ads(all_ads):
 
 # Creates a list for one ad with correct parameters
 def extract_data_ad(ad):
-    ad_id = ad['id']
-    email = ad.get('application_details', {}).get('email', ' ')
-    city = ad.get('workplace_address', {}).get('municipality', ' ')
+    employment_type = ad.get('working_hours_type', {}).get('label', ' ')
+    duration = ad.get('duration', {}).get('label', ' ')
+    publication_date = ad.get('publication_date', ' ')
     occupation = ad.get('occupation', {}).get('label', ' ')
-    return [ad_id, email, city, occupation]
+    county = ad.get('workplace_address', {}).get('region', ' ')
+
+    # Recieve all the experience requirements. 
+    experience = []
+    experience.append(["experience_required", ad.get('experience_required', ' ')])
+    #########   FIXA I FRAMTIDEN    ############
+    experience.append(["work_experience", ad.get('must_have', {}).get('work_experiences', ' ').get('label', ' ')])
+    #############################################
+    experience.append(["education", ad.get('education', ' ')])
+    experience.append(["education_level", ad.get('education_level', ' ')])
+    experience.append(["skills", ad.get('must_have', {}).get('skills', ' ')])
+    experience.append(["language", ad.get('must_have', {}).get('language', ' ')])
+    experience.append(["access_to_own_car", ad.get('access_to_own_car', ' ')])
+    experience.append(["driving_license_required", ad.get('driving_license_required', ' ')])
+    experience.append(["driving_license", ad.get('driving_license', ' ')])
+
+    # seniority
+    return ["platsbanken", employment_type, duration, publication_date, occupation, county, experience]
   
 
 # Main script
