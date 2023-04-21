544fa04b688c73229a9de96c8544877e9d0f2bf4
DrakenDurfors <47025935+DrakenDurfors@users.noreply.github.com>
2023-04-21 13:17:52 +0200

    Update main.py added ledigajobb

--- Project/code/main.py
+++ Project/code/main.py
@@ -2,6 +2,7 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from db import insert
+from ledigajobb import ledigajobb
 from platsbanken import platsbanken
 from linkedIn import linkedIn
 
@@ -14,8 +15,8 @@ platsbanken_list = platsbanken.run()
 insert.send_2d_list(platsbanken_list, database_name)
 
 # LedigaJobb
-# ledigajobb_list = ...
-# InsertIntoDatabase.send_2d_list(ledigajobb_list, database_name)
+ledigajobb_list = ledigajobb.run()
+insert.send_2d_list(ledigajobb_list, database_name)
 
 # LinkedIn
 # linkedIn_list = linkedIn.run()
