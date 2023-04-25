f100e508ce8f4df88673c96d32dfb17954f948de
Klara <79709156+kflt@users.noreply.github.com>
2023-04-25 10:25:08 +0200

    Updated key words in find_seniority

--- Project/code/reqfinder.py
+++ Project/code/reqfinder.py
@@ -2,15 +2,11 @@ import re
 
 # Job ad test variable
 job_ad = """
-
 """
-
-#högskoleutbildning
-#civilingenjörsexamen
 
 # Define regular expressions for bachelor's, master's, and PhD degrees
 def find_req(job_ad):
-    #Keywords to be looked for in the text
+    # Keywords to be looked for in the text
     bachelors_regex = r"bachelor[\']?s degree"
     bachelors_regexa = r"bachelor[\’]?s degree"
     bachelors_regexm = r"bachelor[\’s]? or master’s degree"
@@ -21,7 +17,9 @@ def find_req(job_ad):
     phd_regex = r"phd|doctorate"
     degree_regex = r"degree"
     civ_regex = r"civilingenjör"
+    civ_regexa = r"civilingenjörsexamen or Civilingenjörsexamen"
     eng_regex = r"högskoleingenjör"
+    eng_regexa = r"högskoleingenjörsexamen or Högskoleingenjörsexamen"
     ut_regex = r"utbildning"
     rut_regex = r"relevant utbildning"
     aut_regex = r"akademisk utbildning"
@@ -29,7 +27,7 @@ def find_req(job_ad):
     u_ut_regex = r"universitets utbildning"
     uni_regex = r"university degree"
 
-    #Words to replace the found key words
+    # Words to replace the found key words
     regex_list = [(bachelors_regex,"Bachelor's degree"),
                 (bachelors_regexa,"Bachelor's degree"),
                 (bachelors_regexm,"Bachelor's degree"),
@@ -39,7 +37,9 @@ def find_req(job_ad):
                 (masters_regexa,"Master's degree"),
                 (phd_regex,"PhD"),
                 (civ_regex,"Engineer"),
+                (civ_regexa, "Engineer"),
                 (eng_regex,"Engineer"),
+                (eng_regexa, "Engineer"),
                 (aut_regex,"Academic relevant degree"),
                 (uni_regex,"University degree"),
                 (uut_regex,"University degree"),
@@ -62,8 +62,9 @@ def find_req(job_ad):
 
 
 # Define regular expressions for seniority
+# First occurrence of string in job ad description matches
 def find_seniority(job_ad):
-    #Keywords to be looked for in the text
+    # Keywords to be looked for in the text
     year_one = r"ett års erfarenhet|1 års erfarenhet|one year of experience|1 year of experience"
     year_two = r"två års erfarenhet|2 års erfarenhet|two years of experience|2 years of experience"
     year_three = r"tre års erfarenhet|3 års erfarenhet|three years of experience|3 years of experience"
@@ -78,16 +79,7 @@ def find_seniority(job_ad):
     #year_twenty = r"tjugo års erfarenhet|20 års erfarenhet"
     year = r"några års erfarenhet|a few yers of experience"
 
-#  
-#   for x in range (0,10):
-#   ...
-#   possible automatisering av years of experience
-    
-
-    # TODO: year_interval 4-5 years of experience?
-    #       english text
-    #       etc.
-    
+    # Words to replace the found key words
     regex_year = [(year_one, 1),
                 (year_two, 2),
                 (year_three, 3),
@@ -98,9 +90,12 @@ def find_seniority(job_ad):
                 (year_eight, 8),
                 (year_nine, 9),
                 (year_ten, 10),
-                (year_fifteen, 15),
-                (year_twenty, 20),
-                (year, 2)
+                #(year_fifteen, 15),
+                #(year_twenty, 20),
+                (year, 2),
+                (year_several, 2),
+                (year_par, 2),
+                (year_work, 2)
                 ]
 
     # Search for seniority using regex keywords by first lowercasing the text
