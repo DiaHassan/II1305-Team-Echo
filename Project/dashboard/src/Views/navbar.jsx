2af42b22e00155b981977a8173beb6a27374e3a4
DrakenDurfors <47025935+DrakenDurfors@users.noreply.github.com>
2023-04-25 13:16:44 +0200

    CSS updates

--- Project/dashboard/src/Views/navbar.jsx
+++ Project/dashboard/src/Views/navbar.jsx
@@ -1,24 +1,67 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react';
 import "../style.css";
 
 export default function Navbar() {
-    return(
-      <>
-        <nav>
-            <div>Swedish Talent Observatory</div>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li >
-                <Link to="/">APP_NEEDS_NAME</Link>
-              </li>
-              <li  >
-                <Link to="/about">About</Link>
-              </li>
-            </ul>
-        </nav>
-      </>
-    )
+
+
+  // const [isScrolled, setIsScrolled] = useState(false);
+
+  // useEffect(() => {
+  //   window.addEventListener('scroll', handleScroll);
+  //   return () => {
+  //     window.removeEventListener('scroll', handleScroll);
+  //   };
+  // }, [isScrolled]);
+
+  // // return (
+  // //   <nav className={`navbar ${isScrolled ? 'small' : ''}`}>
+  // //     {/* Navbar content */}
+  // //   </nav>
+  // // );
+
+  // const handleScroll = (e) => {
+
+  // }
+
+  
+  const stickyNav = () => {
+    const [navbar, navbarSet] = useState(false)
+
+    const changeBackground = () => {
+      console.log(window.scrollY)
+      if(window.scrollY > 20) {
+        navbarSet(true)
+      } else {
+        navbarSet(false)
+      }
+    }
+    useEffect(() => {
+      changeBackground()
+      // adding the event when scroll change background
+      window.addEventListener("scroll", changeBackground)
+    })
+
+    {navbar ? "navbar active" : "navbar"}
+  }
+
+  return (
+    <>
+      <nav /*</>className={`navbar ${isScrolled ? 'small' : ''}`}*/>
+        <div>Swedish Talent Observatory</div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li >
+            <Link to="/">APP_NEEDS_NAME</Link>
+          </li>
+          <li  >
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  )
 }
