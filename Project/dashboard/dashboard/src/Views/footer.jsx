9fa0007cc3c1d55f2cda2b0f675d5d0c43eb65c6
Hassan Dia <hassandia_@outlook.com>
2023-04-25 08:42:57 +0200

    Footer update

--- Project/dashboard/dashboard/src/Views/footer.jsx
+++ Project/dashboard/dashboard/src/Views/footer.jsx
@@ -1,10 +1,24 @@
 import React from "react";
-// import {Link} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 export default function Footer() {
     return(
       <>
-        <h1> Lol i am footer </h1>
+        <footer>
+      <div>
+        <h3>Navigation</h3>
+        <ul>
+          <li><Link to="/" className="">Home</Link></li>
+          <li><Link to="/" className="">APP_NEEDS_NAME</Link></li>
+          <li><Link to="/" className="">About</Link></li>
+        </ul>
+      </div>
+      <div>
+        <span>Contact Us:</span>
+        <a href="tel:+1234567890">+1 (234) 567-890</a>
+        <a href="mailto:info@example.com">info@example.com</a>
+      </div>
+    </footer>
       </>
     )
 }
