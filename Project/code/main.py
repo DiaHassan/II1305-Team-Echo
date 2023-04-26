7f1a1e4146e01421235ad54ad93d3232eebf5abc
Hassan Dia <hassandia_@outlook.com>
2023-04-26 10:19:38 +0200

    Statistics update

--- Project/code/main.py
+++ Project/code/main.py
@@ -11,13 +11,13 @@ from linkedIn import linkedIn
 database_name = "Project\db\echo.db"
 
 # Platsbanken
-# platsbanken_list = platsbanken.run()
-# insert.send_2d_list(platsbanken_list, database_name)
+platsbanken_list = platsbanken.run()
+insert.send_2d_list(platsbanken_list, database_name)
 
 # LedigaJobb
 # ledigajobb_list = ledigajobb.run()
 # insert.send_2d_list(ledigajobb_list, database_name)
 
 # LinkedIn
-linkedIn_list = linkedIn.run()
+# linkedIn_list = linkedIn.run()
 # insert.send_2d_list(linkedIn_list, database_name)
