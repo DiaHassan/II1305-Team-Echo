c6b373153c77b10cb104ba60e3bf6037138c985a
EdwinAhl <edwin.ahlstrand@bahnhof.se>
2023-04-27 08:40:09 +0200

    Fixed get_profession_in_conties in extract.py

--- Project/db/extract.py
+++ Project/db/extract.py
@@ -5,13 +5,22 @@ from countyprof import profession_list
 db_path = 'Project\db\echo.db'
 
 
+# [(a,b), (c,d)] --> [[a,b], [c,d]]
+def list_of_tuples_to_2d_list(list_of_tuples):
+    result = []
+    for t in list_of_tuples:
+       t = [t[0], t[1]]
+       result.append(t) 
+    return result    
+
+
 # [[(a,b)], [(c,d)]] --> [[a,b], [c,d]]
 def list_of_list_tuple_to_2d_list(list_of_list_tuple):
     result = []
     for list_tuple in list_of_list_tuple:
-       x = list_tuple.pop()
-       y = [x[0], x[1]]
-       result.append(y) 
+       t = list_tuple.pop()
+       t = [t[0], t[1]]
+       result.append(t) 
     return result     
 
 
@@ -21,10 +30,11 @@ def get_profession_in_counties(profession):
     with sqlite3.connect(db_path) as conn:
         cursor = conn.cursor()
         query = 'SELECT county, count(id) as "sum" FROM job_listing WHERE job_id = (SELECT id FROM job WHERE profession LIKE "%' + profession + '%") AND county IS NOT "null" GROUP BY county'
-        result = cursor.execute(query).fetchall()
-        result.insert(0, profession)  
+        result = cursor.execute(query).fetchall()  
         cursor.close()
     conn.close()
+    result = list_of_tuples_to_2d_list(result)
+    result.insert(0, profession)
     print(result)
     return result
 
@@ -49,4 +59,4 @@ def get_professions_in_county(county):
 # Test
 if __name__ == '__main__':
     get_profession_in_counties('Städare')
-    #get_professions_in_county('Stockholms län')
+    get_professions_in_county('Stockholms län')
