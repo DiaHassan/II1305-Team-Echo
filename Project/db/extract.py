16455a046d3c80cf3e7e2cc4931d0e4c8c8a9fbe
EdwinAhl <edwin.ahlstrand@bahnhof.se>
2023-04-27 14:22:00 +0200

    Restructure in extract.py

--- Project/db/extract.py
+++ Project/db/extract.py
@@ -27,13 +27,36 @@ def list_of_list_tuple_to_2d_list(list_of_list_tuple):
 def get_profession_in_counties(profession):
     with sqlite3.connect(db_path) as conn:
         cursor = conn.cursor()
-        query = 'SELECT jl.county, COUNT(j.id) as job_count FROM job_listing jl LEFT JOIN job j ON jl.job_id = j.id AND j.profession LIKE "%' + profession + '%" WHERE county IS NOT "null" GROUP BY jl.county'
+        query = 'SELECT jl.county, COUNT(j.id) as job_count \
+                FROM job_listing jl \
+                LEFT JOIN job j ON jl.job_id = j.id AND j.profession LIKE "%' + profession + '%" \
+                WHERE county IS NOT "null" \
+                GROUP BY jl.county'
         result = cursor.execute(query).fetchall()  
         cursor.close()
     conn.close()
     result = list_of_tuples_to_2d_list(result)
     result.insert(0, profession)
-    print(type(result))
+    print(result)
+    return result
+
+
+# X-axis: employment type per county
+# Y-axis: variable param of ads in variable profession
+def get_employment_type_per_county(profession, param): 
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
+        query = 'SELECT j.county, j.' + param + ', COUNT(*) as count \
+                FROM job_listing j \
+                JOIN job p ON j.job_id = p.id \
+                WHERE p.profession LIKE "%' + profession + '%" AND ' + param + ' IS NOT + "null"\
+                GROUP BY j.county, j.' + param
+        result = cursor.execute(query).fetchall()
+        cursor.close()
+    conn.close()
+    result = list_of_tuples_to_2d_list(result)
+    result.insert(0, profession)
+    print(result)
     return result
 
 
@@ -44,41 +67,21 @@ def get_professions_in_county(county):
         result = []
         cursor = conn.cursor()
         for profession in profession_list:
-            query = 'SELECT "' + profession +  '" AS profession, count(job_listing.id) as "sum" FROM job_listing INNER JOIN job ON job.id = job_listing.job_id WHERE county = "' + county + '" AND profession LIKE "%' + profession + '%"'
+            query = 'SELECT "' + profession +  '" AS profession, COUNT(job_listing.id) as "sum" \
+                    FROM job_listing INNER JOIN job ON job.id = job_listing.job_id \
+                    WHERE county = "' + county + '" AND profession LIKE "%' + profession + '%"'
             result.append(cursor.execute(query).fetchall())
         cursor.close()
     conn.close()
     result = list_of_list_tuple_to_2d_list(result)
     result.insert(0, county)
-    print(type(result))
-    return result
-
-# X-axis: employment type per county
-# Y-axis: number of ads in variable profession
-def get_employment_type_per_county(profession): 
-    with sqlite3.connect(db_path) as conn:
-        result = []
-        cursor = conn.cursor()
-        query = 'SELECT j.county, j.employment_type, \
-                    CASE WHEN p.profession LIKE "%' + profession + '%" THEN "' + profession + '" ELSE p.profession END AS profession, \
-                    COUNT(*) as count \
-                FROM job_listing j \
-                JOIN job p ON j.job_id = p.id \
-                WHERE p.profession LIKE "%' + profession + '%" AND employment_type IS NOT + "null"\
-                GROUP BY j.county, j.employment_type, \
-                    CASE WHEN p.profession LIKE "%' + profession + '%" THEN "' + profession + '" ELSE p.profession END;'
-        result.append(cursor.execute(query).fetchall())
-        cursor.close()
-    conn.close()
-    result = list_of_tuples_to_2d_list(result)
-    result.insert(0, profession)
     print(result)
     return result
 
 
 # Test
 if __name__ == '__main__':
-    # get_profession_in_counties('Städare')
+    get_profession_in_counties('Städare')
     # get_professions_in_county('Stockholms län')
-    get_employment_type_per_county('Läkare')
+    # get_employment_type_per_county('Läkare', 'employment_type')
 
