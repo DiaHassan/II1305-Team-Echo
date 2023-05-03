4d6e359664f001acc7dd1e9b30161ce3b9f033a1
ZakOra1 <32168076+ZakOra1@users.noreply.github.com>
2023-05-03 16:40:13 +0200

    Added return-statement in get_occupation_ids.py

--- Project/code/platsbanken/get_occupation_id.py
+++ Project/code/platsbanken/get_occupation_id.py
@@ -2,22 +2,23 @@ import codecs      #swedish alphabet
 import requests
 import json
 
+def return_ids():
+    url = "https://data.jobtechdev.se/taxonomy/yrkesben%C3%A4mningar.json"
+    connection = requests.get(url)
 
-url = "https://data.jobtechdev.se/taxonomy/yrkesben%C3%A4mningar.json"
-connection = requests.get(url)
+    # Loads the JSON data into a Python dictionary
+    data = json.loads(connection.text)
+    tuples = data['data']['concepts']
 
-# Loads the JSON data into a Python dictionary
-data = json.loads(connection.text)
-tuples = data['data']['concepts']
+    jobs = ["Ingenjör", "Utvecklare", "Läkare", "Sjuksköterska", "Lärare", "Operatör", "Tekniker", "Elektriker", "Projektledare", "Logistiker"]
+    all_ids = []
 
-jobs = ["Ingenjör", "Utvecklare", "Läkare", "Sjuksköterska", "Lärare", "Operatör", "Tekniker", "Elektriker", "Projektledare", "Logistiker"]
-all_ids = []
+    for job in jobs:
+        ids = []
+        for tuple in tuples:
+            if all(word.lower() in tuple['preferred_label'].lower() for word in job.split()):
+                ids.append(tuple['id'])
+        if len(ids) > 0:
+            all_ids.append(ids)
 
-for job in jobs:
-    ids = []
-    for tuple in tuples:
-        if all(word.lower() in tuple['preferred_label'].lower() for word in job.split()):
-            ids.append(tuple['id'])
-    if len(ids) > 0:
-        all_ids.append(ids)
-print(all_ids)
+    return all_ids
