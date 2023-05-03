9789aaa74fb16750197b530509f6bda4a130196e
EdwinAhl <edwin.ahlstrand@bahnhof.se>
2023-05-03 09:48:52 +0200

    Fixed all extract functions, need ring-function

--- Project/db/Fortabletest.py
+++ Project/db/Fortabletest.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify,request
 # , jsonify, request
 from flask_cors import CORS
-from extract import get_profession_in_counties, get_professions_in_county
+from extract import get_counties_for_profession, get_professions_in_county
 
 # def my_function(arg1, arg2):
 #     # do something with arg1 and arg2
@@ -18,7 +18,7 @@ def endpoint():
 
     data = request.get_json()
     arg1 = data['job']
-    info = get_profession_in_counties(arg1)
+    info = get_counties_for_profession(arg1)
     info.pop(0)
     print(info)
 
