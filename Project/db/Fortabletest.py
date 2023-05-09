2a748e086399359d18dbcec111ff0853f5d4abde
DrakenDurfors <47025935+DrakenDurfors@users.noreply.github.com>
2023-05-09 08:18:28 +0200

    Fixed Concatenation Error

--- Project/db/Fortabletest.py
+++ Project/db/Fortabletest.py
@@ -18,7 +18,7 @@ def endpoint():
 
     data = request.get_json()
     arg1 = data['job']
-    print("Arg1: " + arg1)
+    print("Arg1: ", arg1)
     if(arg1[3]=='Inget val'):
         arg1[3] = 'null'
     info = extract(arg1[0],arg1[1],arg1[2],arg1[3])
