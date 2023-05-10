18ae794663c82b71593c53ff19d69627fb6a7901
ZakOra1 <32168076+ZakOra1@users.noreply.github.com>
2023-05-10 10:42:43 +0200

    Fixed platsbanken bug with void ads

--- Project/code/platsbanken/platsbanken.py
+++ Project/code/platsbanken/platsbanken.py
@@ -51,7 +51,6 @@ def run():
 
     return valid_ads
 
-
 # Retrieves all ads (with given ids) in full json format
 def get_ads(ids):
 
@@ -78,25 +77,16 @@ def get_ads(ids):
     log.info(f"Got {len(list_of_ads)} ads from {url}")
     return list_of_ads
 
- # Removes ads with "null" value in given field
-def remove_void_ads(ads):
-    """
-    Remove ads with county set as "null" from the given list of ads.
 
-    Args:
-        ads (list): A list of ads to filter.
-
-    Returns:
-        list: A list of ads with non-empty fields.
-    """
-
+# Removes ads with "null" value in given field
+def remove_void_ads(ads: list) -> list:
     initial_length = len(ads)
-
+    valid_ads = []
     for ad in ads:
-        if ad[5] == "null":
-            ads.remove(ad)
-    log.info(f"Removed {initial_length - len(ads)} ads out of {initial_length}")
-    return ads
+        if ad[5] != 'null':
+            valid_ads.append(ad)
+    log.info(f"Removed {initial_length - len(valid_ads)} ads out of {initial_length}")
+    return valid_ads
 
 
 # Loads all ads into a list with appropiate parameters
@@ -116,14 +106,6 @@ def extract_duration(duration):
           return char
     return 0
 
-def extract_prerequirement(ads):
-    ad_descriptions = []
-    for ad in ads:
-        ad_descriptions.extend(ad[10])
-    skills = find_req_ai(ad_descriptions)
-    return skills
-
-
 # Creates a list for one ad with correct parameters
 def extract_data_ad(ad, index):
 
@@ -140,9 +122,15 @@ def extract_data_ad(ad, index):
     occupation = index_dict[index]
     county = ad.get('workplace_address', {}).get('region') or 'null'
     date_extracted = datetime.today().strftime('%Y-%m-%d')
-    description = ad.get('description', {}).get('text') or 'null'
+    description_list = ad.get('description', {}).get('text') or 'null'
+    description = ''.join(description_list) if isinstance(description_list, list) else description_list
     prereq = find_req(description)
     years = find_seniority(description)
+
+    if employment_type is not None:
+        employment_type= employment_type.lower()
+    if county is not None:
+        county = county.lower()
 
     # Formatting the publication_date from YYYY-MM-DDTHH:MM:SS to YYYY-MM-DD
     publication_date = publication_date[:10]
@@ -157,8 +145,7 @@ def extract_data_ad(ad, index):
             prereq, 
             years, 
             'null', 
-            date_extracted,
-            description
+            date_extracted
             ]
 
 
