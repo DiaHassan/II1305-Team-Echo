fc20de9ac37af77c5822dbb93e7a57c4dcbf330d
ruisnake <thesnakerl@gmail.com>
2023-06-25 20:38:08 +0200

    Added Öppna/Stäng-buttons
    Added togglable buttons for Instruktioner and Tolkning

--- Project/dashboard/src/Views/statistics.jsx
+++ Project/dashboard/src/Views/statistics.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import Navbar from "./navbar";
 import Footer from "./footer";
 import Tabletest from "../Presenters/Tabletest";
 import Toberem from "../Presenters/ToBeRem";
 
 export default function Stats() {
-    return(
-      <>
-        <Navbar/>
-        <div class="instruktioner">
-          <h2>Instruktioner</h2>
+  const [instruktionerVisible, setInstruktionerVisible] = useState(true);
+  const [tolkningVisible, setTolkningVisible] = useState(true);
+
+  function toggleInstruktioner() {
+    setInstruktionerVisible(!instruktionerVisible);
+  }
+
+  function toggleTolkning() {
+    setTolkningVisible(!tolkningVisible);
+  }
+
+  return(
+    <>
+      <Navbar/>
+      <div class="instruktioner">
+        <h2>
+          Instruktioner
+          <button class="closeInstruktioner" onClick={toggleInstruktioner}>
+            {instruktionerVisible ? "Stäng" : "Öppna"}
+          </button>
+        </h2>
+        {instruktionerVisible && (
           <ul>
             <p>1. Välj en eller flera plattformar att analysera data från.</p>
             <p>2. Välj mellan att analysera flera olika yrken i ett län, eller analysera ett yrke i flera län.</p>
@@ -18,19 +35,26 @@ export default function Stats() {
             <p>5. Välj månad från ett år under datum.</p>
             <p>6. Om filtrerad data önskas, öppna filter och välj ett.</p>
           </ul>
-        </div>
-        <div className="">
-          {/* <Toberem /> */}
-          <Tabletest/>
-        </div>
-        <div class="tolkning">
-          <h2>Hur man tolkar grafen</h2>
-          <ul>
-            <li>På x-axeln är antingen flera yrken över ett län, eller flera län med ett yrke.</li>
-            <li>På y-axeln är antalet för vald kategori, vilket kan vara filtrerat om man valt det.</li>
-          </ul>
-        </div>
-        <Footer/>
-      </>
-    )
+        )}
+      </div>
+      <div className="">
+        {/* <Toberem /> */}
+        <Tabletest/>
+      </div>
+      <div class="tolkning">
+        <h2>
+          Hur man tolkar grafen
+          <button class="closeTolkning" onClick={toggleTolkning}>
+            {tolkningVisible ? "Stäng" : "Öppna"}
+          </button>
+        </h2>
+        {tolkningVisible && (<ul>
+          <li>På x-axeln är antingen flera yrken över ett län, eller flera län med ett yrke.</li>
+          <li>På y-axeln är antalet för vald kategori, vilket kan vara filtrerat om man valt det.</li>
+        </ul>
+        )}
+      </div>
+      <Footer/>
+    </>
+  )
 }
