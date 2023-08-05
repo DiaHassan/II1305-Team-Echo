0dd746db5e3408fde324a14fc70d77edab8bf952
ruisnake <thesnakerl@gmail.com>
2023-08-05 13:40:24 +0200

    Modified commit.py for Zeet

--- Project/db/commit.py
+++ Project/db/commit.py
@@ -2,13 +2,14 @@ import os
 from git import Repo
 
 def git_commit_and_push():
-    repo_path = "path/to/git/repo"
-    ssh_key_path = "C:/Users/username/.ssh/id_rsa"
+    repo_path = os.environ.get("REPO_PATH")
+    ssh_key_path = os.environ.get("SSH_KEY_PATH")
+
     os.environ["GIT_SSH_COMMAND"] = f"ssh -i {ssh_key_path}"
     repo = Repo(repo_path)
-    repo.git.checkout("main")  # Switch to the "main" branch
-    repo.git.add('test.txt')
-    repo.git.commit("-m", "This commit was added through a script")
+    repo.git.checkout("to-be-published")  # Switch to the "main" branch
+    repo.git.add('echo.db')
+    repo.git.commit("-m", "This commit was added through Zeet")
     repo.git.push()
 
 # Main
